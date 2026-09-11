fa860e676cf110ddc5cf77ba75d2896ca6c0c1ed
agropecuarioalivalc-blip <agropecuarioalivalc@gmail.com>
2026-09-11 14:08:18 -0500

    Update prueba.js

--- api/prueba.js
+++ api/prueba.js
@@ -1,6 +1,23 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    exito: true,
-    mensaje: "La función de Vercel funciona correctamente"
-  });
+export default async function handler(req, res) {
+  try {
+    const respuesta = await fetch(
+      "https://script.google.com/macros/s/AKfycbziCfWdKFAXKBOg0vGD68w6fgva9uRuIqQ12KmnQqphaLJxPxjH1EZHa2E_zC9NZavBxQ/exec",
+      {
+        method: "POST"
+      }
+    );
+
+    const texto = await respuesta.text();
+
+    res.status(200).json({
+      exito: true,
+      respuestaAppsScript: texto
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      exito: false,
+      error: error.message
+    });
+  }
 }
